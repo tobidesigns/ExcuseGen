9b18d14290781d2e65432c17722e0f6d0c6b7d5f
Tobi <tobiraheem@gmail.com>
2023-02-02 20:02:41 +0000

    update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["my daughter", "my dog", "my alter-ego", "my bird"];
+let action = ["ate", "crushed", "destroyed", "damaged"];
+let what = ["my homework", "the car", "the cake", "my phone"];
+let when = ["right now", "before class", "last week", "many moons ago"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function getRandomArrayEntry(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.getElementById("excuse").innerHTML =
+    getRandomArrayEntry(who) +
+    " " +
+    getRandomArrayEntry(action) +
+    " " +
+    getRandomArrayEntry(what) +
+    " " +
+    getRandomArrayEntry(when);
+
+  document.querySelector("#btclick").addEventListener("click", () => {
+    window.location.reload();
+  });
 };
